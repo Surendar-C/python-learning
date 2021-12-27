a43510f6f97f984ce9298c65ad126da9a526c585
Surendar C <mcsurendar4code@outlook.com>
2021-12-27 12:12:26 +0530

    digit and alphabet occurrences examples added

--- Lists.py
+++ Lists.py
@@ -27,8 +27,3 @@ print("length:", len(abbreviations))
 print("newly modified list", abbreviations)
 
 
-
-## code to find the number of occurences of an alphabet in a list
-
-words = ["do", "was", "numnber", "order", "apple", "organe", "elephant"]
-find = input("Enter the alpa")
--- findAlphabets.py
+++ findAlphabets.py
@@ -2,4 +2,13 @@
 ## code to find the number of occurences of an alphabet in a list
 
 words = ["do", "was", "numnber", "order", "apple", "organe", "elephant"]
-find = input("Enter the alpa")
+find = input("Enter the alpabet that you want to find\n")
+count = 0
+for word in words:
+    if not type(word) is str:
+        continue;
+    else:
+        lst = list(word)
+        count = count + lst.count(find)
+
+print("Total occurence : ", count)
